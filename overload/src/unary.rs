0cb004117917a743a337d0ff329ebfa4b3b6a483
86maid <134382214+86maid@users.noreply.github.com>
2026-04-16 03:56:46 +0800

    update

---
+++ overload/src/unary.rs
@@ -0,0 +1,21 @@
+#[doc(hidden)]
+#[macro_export(local_inner_macros)]
+macro_rules! _overload_unary {
+    (-, $($t:tt)+) => (_overload_unary_internal!(Neg, neg, $($t)+););
+    (!, $($t:tt)+) => (_overload_unary_internal!(Not, not, $($t)+););
+}
+
+#[doc(hidden)]
+#[macro_export(local_inner_macros)]
+macro_rules! _overload_unary_internal {
+    ($op_trait:ident, $op_fn:ident, $i:ident, $t:ty, $out:ty, $body:block) => {
+        impl std::ops::$op_trait for $t {
+            type Output = $out;
+            #[track_caller]
+            fn $op_fn(self) -> Self::Output {
+                let $i = self;
+                $body
+            }
+        }
+    };
+}
---
+++ overload/tests/unary.rs
@@ -0,0 +1,20 @@
+extern crate overload;
+use overload::overload;
+
+#[derive(PartialEq, Debug)]
+struct A(i32);
+
+#[derive(PartialEq, Debug)]
+struct B(i32);
+
+overload!(- (a: A) -> B { B(-a.0) });
+#[test]
+fn neg() {
+    assert_eq!(-A(3), B(-3));
+}
+
+overload!(! (a: A) -> B { B(!a.0) });
+#[test]
+fn not() {
+    assert_eq!(!A(3), B(!3));
+}
